f068148c24c6938d547e2be3a82e0bea93bec8cb
hyxPeaceful <hyx.peaceful@gmail.com>
2024-06-13 21:33:46 +0800

    创建内容管理模块工程，包括4个工程。一个父工程yanxuexi-content，三个子工程yanxuexi-content-api,yanxuexi-content-service, yanxuexi-content-model

---
+++ yanxuexi-content/yanxuexi-content-model/src/test/java/com/yanxuexi/YanxuexiContentModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanxuexi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanxuexiContentModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
