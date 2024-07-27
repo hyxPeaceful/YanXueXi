632f2e1a4b0039cc399ceb821145b7f6b9423997
hyxPeaceful <hyx.peaceful@gmail.com>
2024-07-27 20:16:50 +0800

    7/27 Day5 01-09 1. Nacos 服务注册和服务配置 2. 搭建媒资服务工程

---
+++ yanxuexi-media/yanxuexi-media-service/src/main/java/com/yanxuexi/media/mapper/MediaFilesMapper.java
@@ -0,0 +1,15 @@
+package com.yanxuexi.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yanxuexi.media.model.po.MediaFiles;
+
+/**
+ * <p>
+ * 媒资信息 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MediaFilesMapper extends BaseMapper<MediaFiles> {
+
+}
---
+++ yanxuexi-media/yanxuexi-media-service/src/main/java/com/yanxuexi/media/mapper/MediaProcessMapper.java
@@ -0,0 +1,15 @@
+package com.yanxuexi.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yanxuexi.media.model.po.MediaProcess;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MediaProcessMapper extends BaseMapper<MediaProcess> {
+
+}
